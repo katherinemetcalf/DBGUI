bad91821f8d4ec98e83725065f884721a93423df
tylerferrara <tylerferrara123@gmail.com>
2018-05-08 19:26:07 -0400

    Added prototyped GUI

---
+++ Prototyping/react_electron_app/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './img/SolvusGlobalLogo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="heading">
+          <h1 style={{"color": "#21A7E0"}}>SOLVUS</h1>
+          <img src={logo} alt="Solvus Global Logo" className="solvusLogo" style={{"width": "60px"}}/>
+          <h1 style={{"color": "#B3B5B8"}}>GLOBAL</h1>
+        </div>
+
+        <div className="login form">
+          <h4>Login</h4>
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <button>Submit</button>
+        </div>
+
+        <div className="register form">
+          <h4>Register</h4>
+            <input type="text" placeholder="username"></input>
+            <input type="password" placeholder="password"></input>
+            <button>Submit</button>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
